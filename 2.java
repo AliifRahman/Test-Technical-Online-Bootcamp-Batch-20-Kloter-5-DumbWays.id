7869cfd37c42fb27e9122254a7f9b7f7cf99ba33
0falsepromise <59392823+0falsepromise@users.noreply.github.com>
2020-12-26 20:56:05 +0700

    soal nomer 2, menggunakan java

---
+++ 2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class 2
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cetak pola():");
+
+        int rows = scanner.nextInt();
+
+        
+        for (int i=rows; i>=1; i--)
+        {
+            for (int k=1; k<=(i * 2) -1; k++) 
+            { 
+                System.out.print("  *"); 
+            } 
+            System.out.println(); 
+
+            for (int j=rows; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            
+        }
+        scanner.close();
+    }
+}
